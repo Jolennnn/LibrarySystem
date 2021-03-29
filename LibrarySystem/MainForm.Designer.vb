bebdaa5df8bd09f38a71f2ac4b439aa0e6ad17db
Julius Makiling <20160010123@my.xu.edu.ph>
2021-03-29 20:27:30 +0800

    MainForm changes

--- LibrarySystem/MainForm.Designer.vb
+++ LibrarySystem/MainForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,39 +20,41 @@ Partial Class MainForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SummaryReportButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.BooksBorrowedButton = New System.Windows.Forms.Button()
+        Me.ReservationsButton = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'SummaryReportButton
         '
-        Me.Button1.Location = New System.Drawing.Point(-5, 112)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(160, 58)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Summary Report"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.SummaryReportButton.Location = New System.Drawing.Point(-5, 112)
+        Me.SummaryReportButton.Name = "SummaryReportButton"
+        Me.SummaryReportButton.Size = New System.Drawing.Size(160, 58)
+        Me.SummaryReportButton.TabIndex = 0
+        Me.SummaryReportButton.Text = "Summary Report"
+        Me.SummaryReportButton.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Bahnschrift Condensed", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(110, 6)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(133, 6)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(501, 58)
         Me.Label1.TabIndex = 2
@@ -62,29 +64,30 @@ Partial Class MainForm
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Bahnschrift Condensed", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(115, 64)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(138, 64)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(214, 25)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Library Management System"
         '
-        'Button2
+        'BooksBorrowedButton
         '
-        Me.Button2.Location = New System.Drawing.Point(-5, 176)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(160, 58)
-        Me.Button2.TabIndex = 0
-        Me.Button2.Text = "Books Borrowed"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BooksBorrowedButton.Location = New System.Drawing.Point(-5, 176)
+        Me.BooksBorrowedButton.Name = "BooksBorrowedButton"
+        Me.BooksBorrowedButton.Size = New System.Drawing.Size(160, 58)
+        Me.BooksBorrowedButton.TabIndex = 0
+        Me.BooksBorrowedButton.Text = "Books Borrowed"
+        Me.BooksBorrowedButton.UseVisualStyleBackColor = True
         '
-        'Button3
+        'ReservationsButton
         '
-        Me.Button3.Location = New System.Drawing.Point(-5, 240)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(160, 58)
-        Me.Button3.TabIndex = 0
-        Me.Button3.Text = "Reservations"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.ReservationsButton.Location = New System.Drawing.Point(-5, 240)
+        Me.ReservationsButton.Name = "ReservationsButton"
+        Me.ReservationsButton.Size = New System.Drawing.Size(160, 58)
+        Me.ReservationsButton.TabIndex = 0
+        Me.ReservationsButton.Text = "Reservations"
+        Me.ReservationsButton.UseVisualStyleBackColor = True
         '
         'Button4
         '
@@ -115,18 +118,19 @@ Partial Class MainForm
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(223, 112)
+        Me.TextBox1.Location = New System.Drawing.Point(191, 112)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(477, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(509, 20)
         Me.TextBox1.TabIndex = 5
         '
         'PictureBox1
         '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(800, 450)
+        Me.PictureBox1.Size = New System.Drawing.Size(853, 500)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 7
         Me.PictureBox1.TabStop = False
@@ -135,29 +139,38 @@ Partial Class MainForm
         '
         Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(12, 6)
+        Me.PictureBox2.Location = New System.Drawing.Point(21, 6)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(97, 100)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 8
         Me.PictureBox2.TabStop = False
         '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(853, 500)
+        Me.Panel1.TabIndex = 9
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(853, 500)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.PictureBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ReservationsButton)
+        Me.Controls.Add(Me.BooksBorrowedButton)
+        Me.Controls.Add(Me.SummaryReportButton)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "MainForm"
         Me.Text = "Father Leoni Memorial School"
@@ -168,15 +181,16 @@ Partial Class MainForm
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents SummaryReportButton As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents BooksBorrowedButton As Button
+    Friend WithEvents ReservationsButton As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel1 As Panel
 End Class
--- LibrarySystem/MainForm.vb
+++ LibrarySystem/MainForm.vb
@@ -1,18 +1,21 @@
 ﻿Public Class MainForm
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BooksBorrowedButton.Click
         BooksBorrowedForm.Show()
         Me.Hide()
     End Sub
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'making labels and some images transparent
+        Panel1.BackColor = Color.FromArgb(150, 0, 58, 97)
         Label1.Parent = PictureBox1
         Label2.Parent = PictureBox1
         Label1.BackColor = Color.Transparent
         Label2.BackColor = Color.Transparent
         PictureBox2.Parent = PictureBox1
         PictureBox2.BackColor = Color.Transparent
-    End Sub
 
+
+    End Sub
 
 End Class
 
