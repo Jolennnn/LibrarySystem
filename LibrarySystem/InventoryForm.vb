baaa38bc5864bcaf531ddce6b4bc80197a0098ad
Julius Makiling <20160010123@my.xu.edu.ph>
2021-04-01 21:42:29 +0800

    Added book form

--- LibrarySystem/InventoryForm.vb
+++ LibrarySystem/InventoryForm.vb
@@ -1,3 +1,6 @@
 ﻿Public Class InventoryForm
-
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AddBookForm.Show()
+        Me.Hide()
+    End Sub
 End Class
--- LibrarySystem/MainForm.vb
+++ LibrarySystem/MainForm.vb
@@ -5,17 +5,12 @@
     End Sub
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'making labels and some images transparent
-        Panel1.BackColor = Color.FromArgb(150, 0, 58, 97)
-        Label1.Parent = PictureBox1
-        Label2.Parent = PictureBox1
-        Label1.BackColor = Color.Transparent
-        Label2.BackColor = Color.Transparent
-        PictureBox2.Parent = PictureBox1
-        PictureBox2.BackColor = Color.Transparent
-
 
     End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        InventoryForm.Show()
+        Me.Hide()
+    End Sub
 End Class
 
