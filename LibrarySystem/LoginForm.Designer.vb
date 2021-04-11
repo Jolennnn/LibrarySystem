6196c8e9e9d7cec83056b20c2f0477e8ec75c88c
HannielGraceEstareja <200720253@my.xu.edu.ph>
2021-04-11 15:55:44 +0800

    gi edit nako ang login form <3

--- LibrarySystem/LoginForm.Designer.vb
+++ LibrarySystem/LoginForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,8 +20,10 @@ Partial Class LoginForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginForm))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -31,24 +33,27 @@ Partial Class LoginForm
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.LMSLabel = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(408, 242)
+        Me.TextBox1.Location = New System.Drawing.Point(599, 355)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(153, 20)
         Me.TextBox1.TabIndex = 0
         '
         'PictureBox1
         '
         Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PictureBox1.Image = Global.LibrarySystem.My.Resources.Resources.IMG_3489
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(800, 450)
+        Me.PictureBox1.Size = New System.Drawing.Size(785, 456)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
@@ -56,7 +61,8 @@ Partial Class LoginForm
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(342, 245)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(537, 358)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(55, 13)
         Me.Label1.TabIndex = 2
@@ -64,16 +70,17 @@ Partial Class LoginForm
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(408, 268)
+        Me.TextBox2.Location = New System.Drawing.Point(599, 381)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(153, 20)
         Me.TextBox2.TabIndex = 0
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(342, 271)
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(539, 384)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(53, 13)
         Me.Label2.TabIndex = 2
@@ -82,6 +89,7 @@ Partial Class LoginForm
         'FLMSLabel
         '
         Me.FLMSLabel.AutoSize = True
+        Me.FLMSLabel.BackColor = System.Drawing.Color.Transparent
         Me.FLMSLabel.Font = New System.Drawing.Font("Bahnschrift Condensed", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FLMSLabel.ForeColor = System.Drawing.Color.White
         Me.FLMSLabel.Location = New System.Drawing.Point(174, 27)
@@ -92,8 +100,8 @@ Partial Class LoginForm
         '
         'PictureBox2
         '
-        Me.PictureBox2.Image = Global.LibrarySystem.My.Resources.Resources.FLMHS_1_
-        Me.PictureBox2.Location = New System.Drawing.Point(35, 26)
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(35, 27)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(121, 121)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -105,7 +113,7 @@ Partial Class LoginForm
         Me.LMSLabel.AutoSize = True
         Me.LMSLabel.Font = New System.Drawing.Font("Bahnschrift Condensed", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LMSLabel.ForeColor = System.Drawing.Color.WhiteSmoke
-        Me.LMSLabel.Location = New System.Drawing.Point(174, 74)
+        Me.LMSLabel.Location = New System.Drawing.Point(176, 69)
         Me.LMSLabel.Name = "LMSLabel"
         Me.LMSLabel.Size = New System.Drawing.Size(242, 29)
         Me.LMSLabel.TabIndex = 3
@@ -113,18 +121,32 @@ Partial Class LoginForm
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(396, 323)
+        Me.Button1.Location = New System.Drawing.Point(677, 407)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 5
         Me.Button1.Text = "Log In"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(35, 160)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(485, 270)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'Timer1
+        '
+        '
         'LoginForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(785, 456)
+        Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.LMSLabel)
@@ -138,6 +160,7 @@ Partial Class LoginForm
         Me.Text = "LoginForm"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -152,4 +175,6 @@ Partial Class LoginForm
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents LMSLabel As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Timer1 As Timer
 End Class
--- LibrarySystem/LoginForm.vb
+++ LibrarySystem/LoginForm.vb
@@ -6,10 +6,56 @@
         FLMSLabel.BackColor = Color.Transparent
         LMSLabel.BackColor = Color.Transparent
         PictureBox2.BackColor = Color.Transparent
+        Timer1.Start()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         MainForm.Show()
     End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim max As Integer = 5000
+        Dim rnd As New Random
+        Dim rand As Integer = rnd.Next(10, max + 1)
+        Dim i As Integer = 1
+        Dim number(max - 1) As Integer
+
+        For i = 0 To max - 1
+            If number(i) = rand Then
+                rand = rnd.Next(1, max + 1)
+                i = -1
+            ElseIf number(i) = 0 Then
+                number(i) = rand
+                rand = rnd.Next(1, max + 1)
+                If i = max - 1 Then
+                    Exit For
+                End If
+                i = -1
+            End If
+        Next
+        Timer1.Interval = number(i)
+        i += 1
+        ChangeImage()
+    End Sub
+
+    Private Sub ChangeImage()
+        Static Dim iImage1 As Integer
+        Select Case iImage1
+            Case 0
+                PictureBox3.Visible = True
+                PictureBox3.Image = Image.FromFile("images/pic1.jpg")
+                iImage1 += 1
+            Case 1
+                PictureBox3.Visible = True
+                PictureBox3.Image = Image.FromFile("images/pic2.jpg")
+                iImage1 += 1
+            Case 2
+                PictureBox3.Visible = True
+                PictureBox3.Image = Image.FromFile("images/pic3.jpg")
+                iImage1 = 0
+        End Select
+    End Sub
+
+
 End Class
